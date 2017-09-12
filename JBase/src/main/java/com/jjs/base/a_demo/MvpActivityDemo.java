900f871df994663bd16a323e2f24986e1482fb6a
jjsCN <994462623@qq.com>
2017-09-12 15:09:41 +0800

    复写setContentView方法，增加butterkinfe支持，再次使用butterkinfe时将不会自动新建onCreate，也许。。。

--- JBase/src/main/java/com/jjs/base/a_demo/MvpActivityDemo.java
+++ JBase/src/main/java/com/jjs/base/a_demo/MvpActivityDemo.java
@@ -1,16 +1,12 @@
 package com.jjs.base.a_demo;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 
 import com.jjs.base.JJsActivity;
-import com.jjs.base.R;
 import com.jjs.base.bean.BusBean;
 import com.jjs.base.bean.HashBean;
-import com.jjs.base.widget.BaseDialog;
 
 import java.util.List;
 
@@ -24,20 +20,7 @@ public class MvpActivityDemo extends JJsActivity<PP> implements VV {
     public void onCreateView(@Nullable Bundle savedInstanceState) {
         mPersenter = new PP(this);//进行实例化，this为PP编写时填入的view接口
         mPersenter.cccc(new HashBean(1));//之后进行具体操作即可
-        Dialog dialog = new BaseDialog(this)
-                .setView(R.layout.layout_load)
-                .setClick(R.id.background_style_ripple, R.id.background_style_default)
-                .setListener(new BaseDialog.OnDialogClickListener() {
-                    @Override
-                    public void onClick(View rootView, View checkView) {
-                        int i = checkView.getId();
-                        if (i == R.id.tv_prompt) {
-                        }
-                    }
-                })
-                .setCancelable(true)
-                .setCanceledOnTouchOutside(false)
-                .create();
+
         new RxSubjectDemo<BusBean>() {
             @Override
             protected void _onSuccess(BusBean busBean) {
--- app/src/main/java/com/jjs/XXXX.java
+++ app/src/main/java/com/jjs/XXXX.java
@@ -1,15 +1,14 @@
 package com.jjs;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import com.jjs.base.http.download.DownLoadObserver;
-import com.jjs.base.http.download.DownloadInfo;
-import com.jjs.base.http.download.DownloadManager;
+import com.jjs.base.widget.BaseDialogFragment;
 
 import butterknife.ButterKnife;
-import io.reactivex.annotations.NonNull;
 
 
 public class XXXX extends Activity {
@@ -19,7 +18,7 @@ public class XXXX extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xxx);
         ButterKnife.bind(this);
-        DownloadManager.getInstance().download("http://gdown.baidu.com/data/wisegame/93812d86a2e7cd82/aiqiyi_80910.apk", new DownLoadObserver() {
+        /*DownloadManager.getInstance().download("http://gdown.baidu.com/data/wisegame/93812d86a2e7cd82/aiqiyi_80910.apk", new DownLoadObserver() {
             @Override
             public void _onNext(DownloadInfo downloadInfo) {
                 Log.e("download", downloadInfo.getProgress() + "/" + downloadInfo.getTotal() + "");
@@ -39,8 +38,47 @@ public class XXXX extends Activity {
             public long setTimeMillis() {
                 return 2000;
             }
-        });
+        });*/
+       /* Dialog dialog = new BaseDialog(this)
+                .setView(R.layout.ccc)
+                .setClick(R.id.tv_title)
+                .setListener(new BaseDialog.OnBaseClickListener() {
+                    @Override
+                    public void onClick(View rootView, View checkView) {
+                        switch (checkView.getId()) {
+                            case R.id.tv_title:
+                                Toast.makeText(XXXX.this, "dianji!!!", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                })
+                .setCancelable(true)
+                .setCanceledOnTouchOutside(false)
+                .create();
+        dialog.show();*/
+        BaseDialogFragment.getInstance(R.layout.ccc, Color.MAGENTA)
+                .setInitViewListener(new BaseDialogFragment.OnInitViewListener() {
+                    @Override
+                    public void onInit(BaseDialogFragment.ViewHolder mHolder) {
+                        mHolder.setText(R.id.tv_title, "xx修改后！！！");
+                        mHolder.setClick(R.id.tv_title);
+                        mHolder.setDismiss(R.id.tv_title);
+                    }
+                })
+                .setBaseClickListener(new BaseDialogFragment.OnBaseClickListener() {
+                    @Override
+                    public void onClick(BaseDialogFragment.ViewHolder mHolder, View v) {
+                        switch (v.getId()) {
+                            case R.id.tv_title:
+                                Toast.makeText(XXXX.this, "!11111111", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
 
+                    }
+                })
+                .hasTranSparent(false)
+                .hasCancelable(false)
+                .show(getFragmentManager(), "1");
     }
 
 }
